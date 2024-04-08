243447967df6ee519c66e433416d14dfe166d444
david <dnjdsxor21@gmail.com>
2024-04-09 01:26:20 +0900

    update: appmount

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ templates = Jinja2Templates(directory="templates")
 app.mount("/js", StaticFiles(directory="templates/js"), name="js") 
 app.mount("/css", StaticFiles(directory="templates/css"), name="css") 
 app.mount("/json", StaticFiles(directory="templates/json"), name="json") 
+app.mount("/templates", StaticFiles(directory="templates"), name="static")
 
 
 BASE_URL = "https://api.odcloud.kr/api"
